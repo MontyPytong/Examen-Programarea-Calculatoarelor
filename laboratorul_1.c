afb414471b876ad5622728ddc730880c7107d7fd
Ene George <ene.georgec@gmail.com>
2021-02-15 17:37:55 +0200

    Update laboratorul_1.c

--- laboratorul_1.c
+++ laboratorul_1.c
@@ -1,18 +1,62 @@
 #include<stdio.h>
+#include<math.h>
 
 int main(){
-        //1.3.1. Întocmiți schema logică pentru rezolvarea ecuației de gradul 1: 𝑎 ∗ 𝑥 + 𝑏 = 0.
-
-       int a,b,rezolvare;
-       printf("Te rog sa introduci termenul a:");
-       scanf("%d", &a);
-       printf("Te rog sa introduci termenul b:");
-       scanf("%d", &b);
-       
-       rezolvare = -b/a;
-       printf("Solutia este : %d",rezolvare);
-
-    
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    //    1.3.1. Întocmiți schema logică pentru rezolvarea ecuației de gradul 1: 𝑎 ∗ 𝑥 + 𝑏 = 0.
+    //
+    //    int a,b,rezolvare;
+    //    printf("Te rog sa introduci termenul a:");
+    //    scanf("%d", &a);
+    //    printf("Te rog sa introduci termenul b:");
+    //    scanf("%d", &b);
+    //   
+    //    rezolvare = -b/a;
+    //    printf("Solutia este : %d",rezolvare);
+    //
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    //
     //    1.3.2. Să se întocmească schema logică pentru calculul expresiei:
     //    𝑓 = 𝑢 + 𝑥^2 −𝑢/𝑥 unde: 𝑢 = 𝑥^2 + 2.34 * sin(𝑥).
+    //
+    //    int f, x, u;
+    //
+    //    printf("te rog sa introduci termenul n.");
+    //    scanf("%d", &x);
+    //
+    //    if(x <= 0){
+    //     printf("termenul introdus trebuie sa fie mai mare ca 0");
+    //    }else{
+    //         //calculam n
+    //         u = pow(x,2) + 2.34 * sin(x);
+    //         //calculam si afisam rezultatul 
+    //         f =  u + pow(x,2) - u/x;
+    //         printf("Solutia este: %d",f);
+    //    }
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    //    
+    //    1.3.3. Să se întocmească schema logică pentru rezolvarea ecuației de gradul doi: 𝑎 ∗ 𝑥^2 + 𝑏 ∗ 𝑥 + 𝑐 = 0.
+    //    int a,b,c,x1,x2;
+    //    printf("Introdu termenul a : ");
+    //    scanf("%d",&a);
+    //    printf("Introdu termenul b : ");
+    //    scanf("%d",&b);
+    //    printf("Introdu termenul c : ");
+    //   scanf("%d",&c);
+    //    int delta = pow(b,2) - 4*a*c;
+    //    if(delta < 0){
+    //        printf("Ecuatia nu are solutii reale.");
+    //    }else if(delta == 0){
+    //        x1 = x2 =  -b/2*a;
+    //        printf("Ecuatia are 2 solutii reale si egale : %d",x1);
+    //    }else if(delta > 0){
+    //        x1 = -b + sqrt(delta)/2*a;
+    //        x2 = -b - sqrt(delta)/2*a;
+    //        printf("Ecuatia are 2 solutii reale si distincte : x1= %d si x2 = %d", x1,x2);
+    //    }else{
+    //        printf("Eroare !");
+    //    }
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    //
+
 }
